ad422f0d05697de688479ede52dd943526f5528e
Gigi <benbout62@gmail.com>
2019-02-24 19:46:22 +0100

    add itoa min in utils

--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,13 @@
 /*   By: bboutoil <bboutoil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 14:22:33 by bboutoil          #+#    #+#             */
-/*   Updated: 2019/02/22 14:46:08 by bboutoil         ###   ########.fr       */
+/*   Updated: 2019/02/24 19:35:23 by bboutoil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	combine_paths(char *path1, char *path2, char *output)
+int		combine_paths(char *path1, char *path2, char *output)
 {
 	
 	const	int s = ft_strlen(path1);
@@ -27,4 +27,20 @@ int	combine_paths(char *path1, char *path2, char *output)
 		ft_strlcat(output, path2, 4097);
 	}
 	return (0);
+}
+
+size_t	ls_itoa_min(unsigned long long nb, char *output)
+{
+	char *tmp;
+
+	tmp = output;
+	if (nb == 0)
+		*tmp++ = '0';
+	while (nb != 0)
+	{
+		*tmp++ = ((nb % 10) + '0');
+		nb /= 10;
+	}
+	*tmp = '\0';
+	return ((size_t)(tmp - output));
 }
