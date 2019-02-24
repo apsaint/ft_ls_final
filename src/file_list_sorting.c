7901706a52b3187e91b8a959a004190546e90297
andre saint-mleux <andresaint-mleux@iMac-de-andre-saint-mleux.local>
2019-02-24 16:20:47 +0100

    starting of the sort algo and explanation

--- src/file_list_sorting.c
+++ src/file_list_sorting.c
@@ -17,9 +17,26 @@ int		compare_by_ascii(t_fstat *f1, t_fstat *f2)
 	return (NOT_IMPLEMENTED_FEATURE);
 }
 
+
+t_flist	*partition(t_flist *f_list, t_flist *t_flist_one, t_flist *t_flist_two, t_options *opt)
+{
+	t_flist	*pivot;
+
+	pivot = f_list;
+//	while ()
+	return (pivot);
+}
+/* <principe de l algorithme:
+	il faut diviser la liste en deux listes autours d une valeur pivot. Une liste contient les valeurs superieurs 
+ au pivot l autre inferieur ou egales.
+Une fois fait on fait le tri sur ces listes avant de les rejoindre en une meme liste.
+Il est plus rapide de trier deux courtes listes qu une longue.
+*/
 void	f_list_qsort(t_flist *f_list, t_options *opt)
 {
 	// exemple d utilisation du ptr de func au cas ou t aurais oublie:
 	// if (opt->sort_func(fl[3], fl[12]) == 1)
 	// par default sort_func = compare by ascii : voir main.c
 }
+
+
