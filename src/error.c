27fb11184e58776a2b96c335bbf329ac3ce468fc
apsaint- <apsaint-@student.42.fr>
2019-03-03 13:38:02 +0100

    error sort function

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: bboutoil <bboutoil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 12:07:57 by bboutoil          #+#    #+#             */
-/*   Updated: 2019/03/03 13:08:10 by apsaint-         ###   ########.fr       */
+/*   Updated: 2019/03/03 13:33:47 by apsaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,43 @@ int	print_path_error(const char *path, int err_num)
 	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putendl_fd(strerror(err_num), STDERR_FILENO);
 	return (0);
+}
+
+int		compare_string(char *s1, char *s2)
+{
+	return ((ft_strcmp(s1, s2) > 0 ? 1 : -1));
+}
+
+int		partition_error(char **error, int min, int max)
+{
+	int		i;
+	int		j;
+	char	*pivot;
+
+	i = min - 1;
+	j = min;
+	pivot = error[max];
+	while (j <= (max - 1))
+	{
+		if (compare_string(error[j], pivot) == -1)
+		{
+			i++;
+			swap(error[i], error[j]);
+		}
+		j++;
+	}
+	swap(error[i + 1], error[max]);
+	return (i + 1);
+}
+
+void	error_qsort(char **error, int max, int min)
+{
+	int		part;
+
+	if (min < max)
+	{
+		part = partition_error(error, min, max);
+		error_qsort(error, part - 1, min);
+		error_qsort(error, max, part + 1);
+	}
 }
